0f4f4d77e24b1b53c5666dacfee616c7c3267511
Buz Carter <buzcarter@gmail.com>
2023-12-30 09:14:18 -0800

    constants (no magic numbers); variable name convention; define variables in smallest scope;

--- src/main.cpp
+++ src/main.cpp
@@ -1,70 +1,89 @@
 #include <Arduino.h>
 
-int station1;
-int station2;
-int station3;
-int Speed;
-int Direction;
-int speedval;
+#define ON HIGH
+#define OFF LOW
+
+const int FORWARD = 0;
+const int REVERSE = 1;
+
+// Arduinao (Output) Digital PWM Pins to Motor Driver
+const int MOTOR_FORWARD = 5;
+const int MOTOR_REVERSE = 6;
+const int MOTOR_SPEED = 3;
+
+// Arduino Analog Input Pins from IR proximity sensors
+const uint8_t TERMINUS_1 = A0;
+const uint8_t TERMINUS_2 = A1;
+const uint8_t MID_STATION_1 = A2;
+
+const uint8_t POTENTIOMETER = A2;
+
+/** Cutoff for IR proximity filter */
+const int SENSOR_THRESHOLD = 500;
+
+int trainSpeed;
+int trainDirection;
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
+  pinMode(MOTOR_FORWARD, OUTPUT);
+  pinMode(MOTOR_REVERSE, OUTPUT);
+  pinMode(MOTOR_SPEED, OUTPUT);
 }
 
 void loop()
 {
-  station1 = analogRead(A0);
-  station2 = analogRead(A1);
-  station3 = analogRead(A2);
-  speedval = analogRead(A3);
-  Speed = map(speedval, 0, 1023, 0, 255);
-  analogWrite(3, Speed);
-  Serial.println(Direction);
-  Serial.println(Speed);
+  int terminus1 = analogRead(TERMINUS_1);
+  int terminus2 = analogRead(TERMINUS_2);
+  int midStation1 = analogRead(MID_STATION_1);
+  int potValue = analogRead(POTENTIOMETER);
+
+  trainSpeed = map(potValue, 0, 1023, 0, 255);
+  analogWrite(MOTOR_SPEED, trainSpeed);
+  Serial.println(trainDirection);
+  Serial.println(trainSpeed);
   delay(200);
 
-  if (station1 < 500)
+  if (terminus1 < SENSOR_THRESHOLD)
   {
-    digitalWrite(5, LOW);
-    digitalWrite(6, LOW);
-    Direction = 0;
+    digitalWrite(MOTOR_FORWARD, OFF);
+    digitalWrite(MOTOR_REVERSE, OFF);
+    trainDirection = FORWARD;
     delay(5000);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, LOW);
+    digitalWrite(MOTOR_FORWARD, ON);
+    digitalWrite(MOTOR_REVERSE, OFF);
 
     delay(1500);
   }
-  if (station2 < 500)
+  if (terminus2 < SENSOR_THRESHOLD)
   {
-    digitalWrite(5, LOW);
-    digitalWrite(6, LOW);
-    Direction = 1;
+    digitalWrite(MOTOR_FORWARD, OFF);
+    digitalWrite(MOTOR_REVERSE, OFF);
+    trainDirection = REVERSE;
     delay(5000);
-    digitalWrite(5, LOW);
-    digitalWrite(6, HIGH);
+    digitalWrite(MOTOR_FORWARD, OFF);
+    digitalWrite(MOTOR_REVERSE, ON);
 
     delay(1500);
   }
 
-  if ((station3 < 500) && (Direction == 1))
+  if ((midStation1 < SENSOR_THRESHOLD) && (trainDirection == REVERSE))
   {
-    digitalWrite(5, LOW);
-    digitalWrite(6, LOW);
+    digitalWrite(MOTOR_FORWARD, OFF);
+    digitalWrite(MOTOR_REVERSE, OFF);
     delay(5000);
-    digitalWrite(5, LOW);
-    digitalWrite(6, HIGH);
+    digitalWrite(MOTOR_FORWARD, OFF);
+    digitalWrite(MOTOR_REVERSE, ON);
     delay(1500);
   }
-  if ((station3 < 500) && (Direction == 0))
+  if ((midStation1 < SENSOR_THRESHOLD) && (trainDirection == FORWARD))
   {
-    digitalWrite(5, LOW);
-    digitalWrite(6, LOW);
+    digitalWrite(MOTOR_FORWARD, OFF);
+    digitalWrite(MOTOR_REVERSE, OFF);
     delay(5000);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, LOW);
+    digitalWrite(MOTOR_FORWARD, ON);
+    digitalWrite(MOTOR_REVERSE, OFF);
     delay(1500);
   }
 }
